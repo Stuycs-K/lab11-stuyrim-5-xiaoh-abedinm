db8704e122cb5f9898c9323b352020a84a976f7a
hax237 <han.xiao5627@gmail.com>
2025-01-14 11:44:19 -0500

    completed most of boss class. need to modify and add certain abilities.

--- Boss.java
+++ Boss.java
@@ -1,15 +1,16 @@
 public class Boss extends Adventurer{
   int rage, rageMax;
 
-  public Boss(String name, int hp, String language){
+  public Boss(String name, int hp){
     super(name,hp);
     rageMax = 12;
     rage = rageMax/2;
   }
-
+/*
   public Boss(String name, int hp){
     this(name,hp);
   }
+*/
 
   public Boss(String name){
     this(name,24);
@@ -41,8 +42,8 @@ public class Boss extends Adventurer{
     int damage = (int)(Math.random()*6)+2;
     other.applyDamage(damage);
     restoreSpecial(2);
-    return this + " attacked "+ other + " and dealt "+ damage +
-    " points of damage. They then take a sip of their coffee.";
+    return this + " underpays "+ other + " and dealt "+ damage +
+    " points of damage. " + other " cannot afford dinner :(";
   }
 
   //hurt or hinder the target adventurer
@@ -50,8 +51,15 @@ public class Boss extends Adventurer{
   public abstract String support(Adventurer other);
 
   //heal or buff self
-  public abstract String support();
+  public abstract String support(){
+    int heal = (int)(Math.random()*6)+2;
+    this.setHP(heal);
+    return this + " degrades "+ other + " and increases self esteem (hp) by "+ heal +
+    " points. ";
+  }
 
   //hurt or hinder the target adventurer, consume some special resource
-  public abstract String specialAttack(Adventurer other);
+  public abstract String specialAttack(Adventurer other){
+
+  };
 }
