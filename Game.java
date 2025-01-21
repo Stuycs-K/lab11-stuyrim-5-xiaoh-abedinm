e28ceacb5e171aade9f039bd722d3ce1ffde6524
Mottaqi Abedin <mottaqiabedin@gmail.com>
2025-01-21 00:07:58 -0500

    cleaned up code, removed unnecessary comments, added comments for easier readability

--- Game.java
+++ Game.java
@@ -11,8 +11,7 @@ public class Game{
     run();
   }
 
-  //Display the borders of your screen that will not change.
-  //Do not write over the blank areas where text will appear or parties will appear.
+  // DRAWING FUNCTIONS
   public static void drawBackground(){
     for (int row = 1; row < 31; row++){
       for (int c = 1; c < 81; c++){
@@ -46,21 +45,18 @@ public class Game{
     }
   }
 
-  //Display a line of text starting at
-  //(columns and rows start at 1 (not zero) in the terminal)
-  //use this method in your other text drawing methods to make things simpler.
-  public static void drawText(String s,int startRow, int startCol){
-    /*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>*/
-    Text.go(startRow, startCol);
-    System.out.print(s);
-    /*<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<*/
+  public static void drawScreen(ArrayList<Adventurer> party, ArrayList<Adventurer> enemies){
+    removeDeadAdventurers(enemies);
+    removeDeadAdventurers(party);
+
+    drawParty(party, HEIGHT-5);
+    drawParty(enemies, 2);
+    drawBackground();
   }
 
-  public static void addTurnMessage(String text) {
-    if (turnHistory.size() >= HISTORY_SIZE) {
-      turnHistory.removeLast();
-    }
-    turnHistory.addFirst(text);
+  public static void drawText(String s,int startRow, int startCol){
+    Text.go(startRow, startCol);
+    System.out.print(s);
   }
 
   public static void drawTurnHistory() {
@@ -70,16 +66,32 @@ public class Game{
     }
   }
 
-  /*Use this method to place text on the screen at a particular location.
-  *When the length of the text exceeds width, continue on the next line
-  *for up to height lines.
-  *All remaining locations in the text box should be written with spaces to
-  *clear previously written text.
-  *@param row the row to start the top left corner of the text box.
-  *@param col the column to start the top left corner of the text box.
-  *@param width the number of characters per row
-  *@param height the number of rows
-  */
+  public static void drawParty(ArrayList<Adventurer> party,int startRow){
+    if (party.isEmpty()) {
+      return;
+    }
+    int width = WIDTH / party.size();
+    for (int i = 0; i < party.size(); i++) {
+      Adventurer temp = party.get(i);
+      int startCol = i * width + 2;
+
+      for (int row = startRow; row < startRow + 3; row++) {
+        Text.go(row, startCol);
+        for (int repeat = 0; repeat < width; repeat++) {
+          System.out.print(" ");
+        }
+      }
+
+      drawText(temp.getName(), startRow, startCol);
+
+      String hp = "HP: " + colorByPercent(temp.getHP(), temp.getmaxHP());
+      drawText(hp, startRow + 1, startCol);
+
+      String special = temp.getSpecialName() + ": " + temp.getSpecial();
+      drawText(special, startRow + 2, startCol);
+    }
+  }
+
   public static void TextBox(int row, int col, int width, int height, String text){
     String[] words = text.split(" ");
     int r = row;
@@ -106,61 +118,17 @@ public class Game{
     }
   }
 
-    //return a random adventurer (choose between all available subclasses)
-    //feel free to overload this method to allow specific names/stats.
-    public static Adventurer createRandomAdventurer(){
-      Adventurer[] randomAdventurers = new Adventurer[] {
-        new ArtMajor(),
-        new CSMajor(),
-        new PhilosophyMajor(),
-      };
-      int randomIndex = (int) (Math.random()*randomAdventurers.length);
-      return randomAdventurers[randomIndex];
+  // ADDING TO PREVIOUS TURNS LIST
+  public static void addTurnMessage(String text) {
+    if (turnHistory.size() >= HISTORY_SIZE) {
+      turnHistory.removeLast();
     }
+    turnHistory.addFirst(text);
+  }
 
-    /*Display a List of 2-4 adventurers on the rows row through row+3 (4 rows max)
-    *Should include Name HP and Special on 3 separate lines.
-    *Note there is one blank row reserved for your use if you choose.
-    *Format:
-    *Bob          Amy        Jun
-    *HP: 10       HP: 15     HP:19
-    *Caffeine: 20 Mana: 10   Snark: 1
-    * ***THIS ROW INTENTIONALLY LEFT BLANK***
-    */
-    public static void drawParty(ArrayList<Adventurer> party,int startRow){
-      if (party.isEmpty()) {
-        return;
-      }
-      int width = WIDTH / party.size();
-      for (int i = 0; i < party.size(); i++) {
-        Adventurer temp = party.get(i);
-        int startCol = i * width + 2;
-
-        for (int row = startRow; row < startRow + 3; row++) {
-          Text.go(row, startCol);
-          for (int repeat = 0; repeat < width; repeat++) {
-            System.out.print(" ");
-          }
-        }
-
-        drawText(temp.getName(), startRow, startCol);
-
-        String hp = "HP: " + colorByPercent(temp.getHP(), temp.getmaxHP());
-        drawText(hp, startRow + 1, startCol);
-
-        String special = temp.getSpecialName() + ": " + temp.getSpecial();
-        drawText(special, startRow + 2, startCol);
-      }
-    }
-
-
-  //Use this to create a colorized number string based on the % compared to the max value.
+  // COLOR FUNCTIONS
   public static String colorByPercent(int hp, int maxHP){
     String output = String.format("%2s", hp+"")+"/"+String.format("%2s", maxHP+"");
-    //COLORIZE THE OUTPUT IF HIGH/LOW:
-    // under 25% : red
-    // under 75% : yellow
-    // otherwise : white
     double hpPercent = (double) hp / maxHP;
     if (hpPercent < 0.25) {
       return(Text.colorize(output, Text.RED));
@@ -173,6 +141,7 @@ public class Game{
     }
   }
 
+  // ADVENTURER FUNCTIONS
   public static Boolean isDead(Adventurer adventurer){
     return (adventurer.getHP() <= 0); 
   }
@@ -185,26 +154,26 @@ public class Game{
       }
     }
   }
-  public static void drawScreen(ArrayList<Adventurer> party, ArrayList<Adventurer> enemies){
-    removeDeadAdventurers(enemies);
-    removeDeadAdventurers(party);
 
-    drawParty(party, HEIGHT-5);
-    drawParty(enemies, 2);
-    drawBackground();
+  public static Adventurer createRandomAdventurer(){
+    Adventurer[] randomAdventurers = new Adventurer[] {
+      new ArtMajor(),
+      new CSMajor(),
+      new PhilosophyMajor(),
+    };
+    int randomIndex = (int) (Math.random()*randomAdventurers.length);
+    return randomAdventurers[randomIndex];
   }
 
+  // USER INPUT
   public static String userInput(Scanner in, int start){
       int row = 29;
       int col = 2 + start;
-      //Move cursor to prompt location
       Text.go(row,col);
 
-      //show cursor
       Text.showCursor();
       String input = in.nextLine();
 
-      //clear the text that was written
       Text.go(row,col);
       for (int i = 0; i < 78-start; i++) {
         System.out.print(" ");
@@ -215,6 +184,7 @@ public class Game{
       return input;
   }
 
+  // START AND END GAME
   public static void quit(){
     Text.clear();
     Text.reset();
@@ -226,9 +196,10 @@ public class Game{
     Text.hideCursor();
     Text.clear();
 
+    // ENEMY ADVENTURERS
     ArrayList<Adventurer> enemies = new ArrayList<Adventurer>();
 
-    // List of CEO names, can remove and just have "Boss" as the name
+    // LIST OF BOSS NAMES
     String[] ceoNames = {
       "Elon Musk", "Jeff Bezos", "Tim Cook", "Sundar Pichai", "Satya Nadella", 
       "Mark Zuckerberg", "Larry Page", "Sergey Brin", "Warren Buffett", 
@@ -247,8 +218,7 @@ public class Game{
       }
     }
 
-    //Adventurers you control:
-    //Make an ArrayList of Adventurers and add 2-4 Adventurers to it.
+    // PARTY ADVENTURERS
     ArrayList<Adventurer> party = new ArrayList<>();
     Scanner in = new Scanner(System.in);
 
@@ -318,41 +288,30 @@ public class Game{
       }
     }
 
+    // PRE-LOOP
     Text.clear();
-
     boolean partyTurn = true;
     int whichPlayer = 0;
     int whichOpponent = 0;
     int turn = 0;
-    String input = "";//blank to get into the main loop.
-    //Draw the window border
+    String input = "";
+    drawScreen(party, enemies);
+    String prompt = "Enter command for " + party.get(whichPlayer).getName() + " (attack #/special #/support #/quit): ";
+    TextBox(29, 2, 77, 1, prompt);
 
-    //You can add parameters to draw screen!
-    drawScreen(party, enemies);//initial state.
-
-    //Main loop
-
-    //display this prompt at the start of the game.
-
-    String preprompt = "Enter command for " + party.get(whichPlayer).getName() + " (attack #/special #/support #/quit): ";
-    TextBox(29, 2, 77, 1, preprompt);
-
+    //MAIN GAME LOOP
     while(! (input.equalsIgnoreCase("q") || input.equalsIgnoreCase("quit"))){
       if (whichPlayer >= party.size()) {
         whichPlayer = 0;
       }
-      //Read user input//YOUR CODE HERE
-      input = userInput(in, preprompt.length());
 
-      //example debug statment
-      // TextBox(24,2,1,78,"input: "+input+" partyTurn:"+partyTurn+ " whichPlayer="+whichPlayer+ " whichOpp="+whichOpponent );
+      input = userInput(in, prompt.length());
 
-      //display event based on last turn's input
+      // PARTY TURN
       if(partyTurn){
         boolean breakOut = false;
         boolean validInput = false;
         while (validInput == false){
-          //Process user input for the last Adventurer:
           if(input.startsWith("attack ") || input.startsWith("a ")){
             String[] temp = input.split(" ");
             int num = Integer.valueOf(temp[1])-1;
@@ -409,9 +368,8 @@ public class Game{
             breakOut = true;
             break;
           }
-          //re-asks for input
           else{
-            String errMessage = "INVALID INPUT. Enter command for " + party.get(whichPlayer).getName() + " (attack #/special #/support #/quit): ";
+            String errMessage = "INVALID INPUT. Enter command for " + party.get(whichPlayer).getName() + " (attack/special/support/quit): ";
             TextBox(29, 2, 77, 1, errMessage);
             input = userInput(in, errMessage.length());
           }
@@ -419,24 +377,19 @@ public class Game{
         if (breakOut == true){
           break;
         }
-        //You should decide when you want to re-ask for user input
-        //If no errors:
+
         whichPlayer++;
 
         if(whichPlayer < party.size()){
-          //This is a player turn.
-          //Decide where to draw the following prompt:
-          preprompt = "Enter command for " + party.get(whichPlayer).getName() + " (attack #/special #/support #/quit): ";
-          TextBox(29, 2, 77, 1, preprompt);
+          prompt = "Enter command for " + party.get(whichPlayer).getName() + " (attack #/special #/support #/quit): ";
+          TextBox(29, 2, 77, 1, prompt);
         }else{
-          //This is after the player's turn, and allows the user to see the enemy turn
-          //Decide where to draw the following prompt:
-          String prompt = "Press enter to see enemy team's turn";
+          prompt = "Press enter to see enemy team's turn";
           TextBox(29, 2, 77, 1, prompt);
           partyTurn = false;
           whichOpponent = 0;
         }
-        //done with one party member
+      // ENEMY TURN
       }else{
         Adventurer attacker = enemies.get(whichOpponent);
         int action = (int) (Math.random()*3);
@@ -464,14 +417,15 @@ public class Game{
 
         if(whichOpponent >= enemies.size()){
           whichPlayer = 0;
-          turn++;
           whichOpponent=0;
           partyTurn=true;
-          String prompt = "Enter command for " + party.get(whichPlayer).getName() + " (attack #/special #/support #/quit): ";
+          turn++;
+          prompt = "Enter command for " + party.get(whichPlayer).getName() + " (attack #/special #/support #/quit): ";
           TextBox(29,2,77,1,prompt);
         }
       }
 
+      // PREPARE FOR NEXT TURN
       removeDeadAdventurers(enemies);
       removeDeadAdventurers(party);
 
@@ -482,7 +436,7 @@ public class Game{
           Text.go(12, 10);
           System.out.println(Text.colorize("You Win! All enemies are defeated.", Text.GREEN));
           Text.go(14, 10);
-          System.out.println(Text.colorize("Enter any key to exit.", Text.YELLOW));
+          System.out.println(Text.colorize("Press Enter to exit.", Text.YELLOW));
           new Scanner(System.in).nextLine();
           return;
       } else if (party.isEmpty()) {
@@ -492,18 +446,16 @@ public class Game{
         Text.go(12, 10);
         System.out.println(Text.colorize("You Lose! All party members are dead.", Text.RED));
         Text.go(14, 10);
-        System.out.println(Text.colorize("Enter any key to exit.", Text.YELLOW));
+        System.out.println(Text.colorize("Press Enter to exit.", Text.YELLOW));
         new Scanner(System.in).nextLine();
         return;
         }
-      //display the updated screen after input has been processed.
+
       drawScreen(party, enemies);
       drawTurnHistory();
 
-    }//end of main game loop
-
-
-    //After quit reset things:
+    }
+    // END OF MAIN GAME LOOP
     quit();
   }
 }
